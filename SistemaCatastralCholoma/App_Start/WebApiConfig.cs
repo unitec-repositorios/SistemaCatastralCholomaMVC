ef4ae22879cef9f32139933979e4e16c893abe53
Usuario <andrefigueroakilgore@gmail.com>
2019-05-08 01:04:38 -0600

    Controller finalizado

--- SistemaCatastralCholoma/App_Start/WebApiConfig.cs
+++ SistemaCatastralCholoma/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace SistemaCatastralCholoma
 {
@@ -19,6 +20,10 @@ namespace SistemaCatastralCholoma
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
         }
     }
 }
