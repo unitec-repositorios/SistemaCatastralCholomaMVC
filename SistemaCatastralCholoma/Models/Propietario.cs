8456d18c53a5bf1a1a3005098f39b4f6e482d90f
Raul Alcantara <raul.alcantara@grupoleitz.com>
2019-05-22 02:33:57 -0600

    created PropietarioController endpoints

--- SistemaCatastralCholoma/Models/Propietario.cs
+++ SistemaCatastralCholoma/Models/Propietario.cs
@@ -8,11 +8,27 @@ namespace SistemaCatastralCholoma.Models
     public class Propietario
     {
         public string id { get; set; }
-        public string nombre { get; set; }
+        public string nombres { get; set; }
         public string apellidos { get; set; }
-        public string teledono { get; set; }
+        public string telefono { get; set; }
         public string rtn { get; set; }
         public char sexo { get; set; }
         public string nacionalidad { get; set; }
+
+        public Propietario()
+        {
+
+        }
+
+        public Propietario(string id, string nombres, string apellidos, string telefono, string rtn, char sexo, string nacionalidad)
+        {
+            this.id = id;
+            this.nombres = nombres;
+            this.apellidos = apellidos;
+            this.telefono = telefono;
+            this.rtn = rtn;
+            this.sexo = sexo;
+            this.nacionalidad = nacionalidad;
+        }
     }
 }
