e0e99833062799284cf8ed053cb5ce07a7e5ce53
Usuario <andrefigueroakilgore@gmail.com>
2019-09-14 23:37:10 -0600

    Added a controller for maintenance of Propietarios

---
+++ SistemaCatastralCholoma/Controllers/manPropietariosController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SistemaCatastralCholoma.Models;
+using System.Data.SqlClient;
+
+namespace SistemaCatastralCholoma.Controllers
+{
+    public class manPropietariosController : ApiController
+    {
+        private SqlConnection conn = WebApiConfig.conn();
+
+        [HttpGet]
+        public HttpResponseMessage listManPropietarios()
+        {
+            List<mantenimientoPropietarios> mp = new List<mantenimientoPropietarios>();
+            try
+            {
+                conn.Open();
+                SqlCommand query = conn.CreateCommand();
+                query.CommandText = "select * from bkmilcp6nvs1hgkadyz6.mantenimientoPropietarios";
+                SqlDataReader reader = query.ExecuteReader();
+                mantenimientoPropietarios tmp;
+                while (reader.Read())
+                {
+                    tmp = new mantenimientoPropietarios();
+                    tmp.id = reader.GetInt32(2);
+                    tmp.sexo = reader.GetString(0);
+                    tmp.nacionalidad = reader.GetString(1);
+
+                    mp.Add(tmp);
+                }
+                conn.Close();
+                var response = Request.CreateResponse(HttpStatusCode.OK, mp);
+                return response;
+            }
+            catch(SqlException e)
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                var response = Request.CreateResponse(HttpStatusCode.BadRequest, e.Message.ToString());
+                return response;
+            }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage crearMP(mantenimientoPropietarios mp)
+        {
+            try
+            {
+                conn.Open();
+                SqlCommand query = conn.CreateCommand();
+                query.CommandText = "insert into bkmilcp6nvs1hgkadyz6.mantenimientoPropietarios values (@sexo, @nacionalidad, @id)";
+
+                query.Prepare();
+                query.Parameters.AddWithValue("@sexo", mp.sexo);
+                query.Parameters.AddWithValue("@nacionalidad", mp.nacionalidad);
+                query.Parameters.AddWithValue("@id", mp.id);
+                query.ExecuteNonQuery();
+
+                conn.Close();
+                var response = Request.CreateResponse(HttpStatusCode.OK, mp);
+                return response;
+            }
+            catch (SqlException e)
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                var response = Request.CreateResponse(HttpStatusCode.BadRequest, e.Message.ToString());
+                return response;
+            }
+        }
+
+        [HttpPut]
+        public HttpResponseMessage modificarMP(int id, mantenimientoPropietarios mp)
+        {
+            try
+            {
+                conn.Open();
+                SqlCommand query = conn.CreateCommand();
+                query.CommandText = "update bkmilcp6nvs1hgkadyz6.mantenimientoPropietarios set sexo = @sexo, nacionalidad = @nacionalidad where id = @id";
+
+                query.Prepare();
+                query.Parameters.AddWithValue("@sexo", mp.sexo);
+                query.Parameters.AddWithValue("@nacionalidad", mp.nacionalidad);
+                query.ExecuteNonQuery();
+
+                conn.Close();
+                var response = Request.CreateResponse(HttpStatusCode.OK, mp);
+                return response;
+
+            }
+            catch (SqlException e)
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                var response = Request.CreateResponse(HttpStatusCode.BadRequest, e.Message.ToString());
+                return response;
+            }
+        }
+
+        [HttpDelete]
+        public HttpResponseMessage deleteMP(int id)
+        {
+            try
+            {
+                conn.Open();
+                SqlCommand query = conn.CreateCommand();
+                query.CommandText = "delete from bkmilcp6nvs1hgkadyz6.mantenimientoPropietarios where id = @id";
+
+                query.Prepare();
+                query.Parameters.AddWithValue("@id", id);
+                query.ExecuteNonQuery();
+
+                conn.Close();
+
+                var response = Request.CreateResponse(HttpStatusCode.NoContent);
+                return response;
+            }
+            catch (SqlException e)
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                var response = Request.CreateResponse(HttpStatusCode.BadRequest, e.Message.ToString());
+                return response;
+            }
+        }
+
+    }
+}
